0a0e1e94133a1b01a9628aa834ff13d1570a226d
anushadevelopment <79033329+anushadevelopment@users.noreply.github.com>
2021-11-21 21:11:40 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello'
+
+@app.route('/calc')
+def calc():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    return str(sum(a,b))
+
+def sum(a,b):
+    return a+b
+
+if __name__ == '__main__':
+    app.run(debug=True)
